a8e4b6cdef573150a26e0bddd8b47b5f30e9743a
ATik HaSan <112861523+atikhasan27000@users.noreply.github.com>
2022-09-09 12:31:54 -0700

    Add files via upload

---
+++ HTML TO SQL.php
@@ -0,0 +1,91 @@
+<!-- Connection -->
+<?php
+    $con = mysqli_connect('localhost', 'root');
+    mysqli_select_db($con,'databesh');
+?>
+<!--// Connection -->
+
+<!-- SQL Datebesh To PHP File order by DESC -->
+<?php
+  $query     = " SELECT `id`, `addedOn` FROM `mydb` order by `addedOn` DESC ";
+  $queryfire = mysqli_query($con, $query);
+  $num       = mysqli_num_rows($queryfire);
+  if($num > 0) {
+      while($date = mysqli_fetch_array($queryfire)){
+?>
+
+<?php echo "?id=".$date["id"]?>
+<?php echo $date["addedOn"]?>
+<?php echo strip_tags(substr($date["details"], 0,50))."..."?>
+<?php }}?>
+<!--// SQL Datebesh To PHP File order by DESC -->
+
+<!-- SQL Datebesh To PHP File order by ASC -->
+<?php
+    $id = $_REQUEST['id'];
+	if(empty($id)) {
+        header("location:index.php");
+    }
+    $query     = " SELECT `id`, `addedOn` FROM `mydb` WHERE id=$id order by id ASC ";
+    $queryfire = mysqli_query($con, $query);
+    $num       = mysqli_num_rows($queryfire);
+    if($num > 0) {
+        while($date = mysqli_fetch_array($queryfire)){
+?>
+
+<?php echo $date["addedOn"]?>
+
+<?php }}?>
+<!--// SQL Datebesh To PHP File order by ASC -->
+
+<!-- Total Number of Data (Row Count) in Database in php -->
+<?php
+	$query     = "SELECT id FROM mydb ORDER BY id";
+	$query_run = mysqli_query($con, $query);
+	$row       = mysqli_num_rows($query_run);
+		echo ''.$row.'';
+?>
+<!--// Total Number of Data (Row Count) in Database in php -->
+
+<!-- HTML Form To SQL Databesh -->
+<?php
+if ($con->connect_error) {
+    die("Connection Failed." . $con->connect_error);
+}
+
+if (isset($_POST['fast_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['phone'])) {
+    $fast_name       = $_POST['fast_name'];
+    $last_name       = $_POST['last_name'];
+    $email           = $_POST['email'];
+    $phone           = $_POST['phone'];
+    $sql             = "INSERT INTO mytable(fast_name, last_name, email, phone) VALUES('$fast_name','$lastName','$email','$phone')";
+    if (mysqli_query($con, $sql)) {
+        // echo '<h3>Data instat</h3>';
+        header("Location: index.php");
+    };
+}
+?>
+<!--// HTML Form To SQL Databesh -->
+
+<!-- Img Upload In Database -->
+<?php
+if ($con->connect_error) {
+    die("Connection Failed." . $con->connect_error);
+}
+if (isset($_POST['title'])) {
+    $title        = $_POST['title'];
+
+    $imgname      = $_FILES['img'] ['name'];
+    $tmpname      = $_FILES['img'] ['tmp_name'];
+    $uploc        = 'images/'.$imgname;
+    $sql          = "INSERT INTO bloglist(title, img) VALUES('$title','$imgname')";
+    if (mysqli_query($con, $sql)) {
+		move_uploaded_file($tmpname,$uploc);
+        // echo '<h3>Data instat</h3>';
+		header("Location: index.php");
+    };
+}
+?>
+
+<form action="" method="POST" enctype="multipart/form-data"></form>
+<!--// Img Upload In Database -->
